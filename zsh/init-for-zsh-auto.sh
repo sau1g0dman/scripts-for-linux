c2a6c578e00a01a055d46f76a8175b9b18dcde1b
sau1g0dman <128480732+sau1g0dman@users.noreply.github.com>
2024-03-26 20:12:40 +0800

    Update init-for-zsh-auto.sh

--- zsh/init-for-zsh-auto.sh
+++ zsh/init-for-zsh-auto.sh
@@ -80,7 +80,7 @@ install_zsh_plugins() {
     echo "安装Zsh插件..."
     git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
     git clone https://github.com/zsh-users/zsh-syntax-highlighting "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
-    git clone https://github.com/MichaelAquilina/zsh-you-should-use.git $ZSH_CUSTOM/plugins/you-should-use
+    git clone https://github.com/MichaelAquilina/zsh-you-should-use.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/you-should-use
     curl -fsSL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
     echo "Zsh插件安装完成。"
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
